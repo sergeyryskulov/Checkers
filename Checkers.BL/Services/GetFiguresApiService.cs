26c750174371b210b9c14b9b0c7f3717b140843d
Рыскулов Сергей Николаевич <s.riskulov@cft.ru>
2021-11-10 22:29:01 +0700

    рефакторинг

---
+++ Checkers.BL/Services/GetFiguresApiService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ckeckers.DAL.Repositories;
+
+namespace Checkers.BL.Services
+{
+    public class GetFiguresApiService
+    {
+        private BoardRepository _boardRepository;
+    }
+}
