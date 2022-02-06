18a547f9560f4a9eddd7af231682951b3e5d646f
Рыскулов Сергей Николаевич <s.riskulov@cft.ru>
2022-02-06 21:52:08 +0700

    юнит тест

---
+++ Checkers.BLTests/Repositories/BoardRepositoryTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ckeckers.DAL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ckeckers.DAL.Repositories.Tests
+{
+    [TestClass()]
+    public class BoardRepositoryTests
+    {
+        [TestMethod()]
+        public void SaveTest()
+        {
+            var boardRepository = new BoardRepository();
+
+            boardRepository.Save("testkey", "testvalue");
+
+            var actual = boardRepository.Load("testkey");
+
+            Assert.AreEqual("testvalue", actual);
+
+
+        }
+
+        [TestMethod()]
+        public void LoadTest()
+        {
+            var boardRepository = new BoardRepository();
+
+            var defaultData = boardRepository.Load("testkey");
+
+            Assert.AreEqual('1', defaultData[0]); ;            
+        }
+    }
+}
