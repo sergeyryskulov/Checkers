2fbe106996dcf653acec2c36c2491f79d8d5e769
Рыскулов Сергей Николаевич <s.riskulov@cft.ru>
2022-05-03 19:56:20 +0700

    рефакторинг

--- Checkers.BLTests/Models/VectorTests.cs
+++ Checkers.BLTests/Models/VectorTests.cs
@@ -36,7 +36,47 @@ namespace Checkers.BL.Models.Tests
             var actualString = vector.ToString();
 
             Assert.AreEqual("LeftTop (2)", actualString);
+
+        }
+
+        [TestMethod()]
+        public void EqualsTest()
+        {
+            var vector1 = new Vector()
+            {
+                Direction = Constants.Enums.Direction.LeftTop,
+                Length = 2
+            };
+
+            var vector2 = new Vector()
+            {
+                Direction = Constants.Enums.Direction.LeftTop,
+                Length = 2
+            };
+
+            var actual = vector1.Equals(vector2);
             
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod()]
+        public void NotEqualsTest()
+        {
+            var vector1 = new Vector()
+            {
+                Direction = Constants.Enums.Direction.LeftTop,
+                Length = 2
+            };
+
+            var vector2 = new Vector()
+            {
+                Direction = Constants.Enums.Direction.RightBottom,
+                Length = 2
+            };
+
+            var actual = vector1.Equals(vector2);
+
+            Assert.IsFalse(actual);
         }
     }
 }
