a5b320dc8ec5e0699e9db403432bbcc67adc2a6c
Рыскулов Сергей Николаевич <s.riskulov@cft.ru>
2022-05-10 19:35:22 +0700

    дизайн шашек

--- Checkers/wwwroot/pages/board/boardDrawer.js
+++ Checkers/wwwroot/pages/board/boardDrawer.js
@@ -24,8 +24,8 @@ var BoardDrawer = /** @class */ (function () {
         }, 300, onComplete);
     };
     BoardDrawer.prototype.drawFigure = function (coord, figure) {
-        var divFigure = '<div id=f$coord class=figure>$figure</div>';
-        $('#s' + coord).html(divFigure.replace('$coord', '' + coord).replace('$figure', this.gettChessSymbol(figure)));
+        var divFigure = '<div id=f$coord class="figure figure_$figure"></div>';
+        $('#s' + coord).html(divFigure.replace('$coord', '' + coord).replace('$figure', figure));
         $('.figure').draggable();
     };
     BoardDrawer.prototype.setDropFigureOnSquareHandler = function (dropCallback) {
@@ -42,19 +42,9 @@ var BoardDrawer = /** @class */ (function () {
     };
     BoardDrawer.prototype.gettChessSymbol = function (figure) {
         switch (figure) {
-            case 'K': return '&#9812;';
             case 'Q': return '&#9813;';
-            case 'R': return '&#9814;';
-            case 'B': return '&#9815;';
-            case 'N': return '&#9816;';
             case 'P': return '&#9817;';
-            case 'k': return '&#9818;';
-            case 'q': return '&#9819;';
-            case 'r': return '&#9820;';
-            case 'b': return '&#9821;';
-            case 'n': return '&#9822;';
             case 'p': return '&#9823;';
-            case 'k': return '&#9824;';
             default:
         }
         return '';
--- Checkers/wwwroot/pages/board/boardDrawer.ts
+++ Checkers/wwwroot/pages/board/boardDrawer.ts
@@ -34,9 +34,8 @@
 
     public drawFigure(coord, figure) {
 
-        let divFigure = '<div id=f$coord class=figure>$figure</div>';
-        $('#s' + coord).html(divFigure.replace('$coord', '' + coord).replace('$figure',
-            this.gettChessSymbol(figure)));
+        let divFigure = '<div id=f$coord class="figure figure_$figure"></div>';
+        $('#s' + coord).html(divFigure.replace('$coord', '' + coord).replace('$figure', figure));
 
         $('.figure').draggable();
     }
@@ -58,19 +57,10 @@
 
     private gettChessSymbol(figure: string): string {
         switch (figure) {
-        case 'K': return '&#9812;';
         case 'Q': return '&#9813;';
-        case 'R': return '&#9814;';
-        case 'B': return '&#9815;';
-        case 'N': return '&#9816;';
         case 'P': return '&#9817;';
-        case 'k': return '&#9818;';
-        case 'q': return '&#9819;';
-        case 'r': return '&#9820;';
-        case 'b': return '&#9821;';
-        case 'n': return '&#9822;';
         case 'p': return '&#9823;';
-        case 'k': return '&#9824;';
+        
         default:
         }
         return '';
