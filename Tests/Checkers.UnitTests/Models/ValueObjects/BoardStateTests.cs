346cd130aefec7a412f542e2757f83c0eac0c458
Рыскулов Сергей Николаевич <s.riskulov@cft.ru>
2022-10-31 21:12:46 +0700

    юнит тест

---
+++ Tests/Checkers.UnitTests/Models/ValueObjects/BoardStateTests.cs
@@ -0,0 +1,20 @@
+﻿using Checkers.Core.Constants;
+using Checkers.Core.Models.ValueObjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Checkers.UnitTests.Models.ValueObjects
+{
+    [TestClass()]
+    public class BoardStateTests
+    {
+        [TestMethod()]
+        public void GetHashCodeTest()
+        {
+            var boardState = new BoardState("", Turn.White);
+
+            var actual = boardState.GetHashCode();
+
+            Assert.AreEqual(actual, "".GetHashCode() + (int)Turn.White);
+        }
+    }
+}
