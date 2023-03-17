f09bdc0369e9bc9b42ed0b18ab9ff2fd1104ad7b
Рыскулов Сергей Николаевич <s.riskulov@cft.ru>
2023-03-17 22:15:06 +0700

    рефакторинг

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using OfficeOpenXml;
+
+namespace ClassLibrary1
+{
+    public class TestClass1Service
+    {
+
+        public TestClass1Service(TestClass2Service x2)
+        {
+           ExcelWorkbook s;
+        }
+    }
+
+   
+    internal class TestClass2Service 
+    {
+        public TestClass2Service()
+        {
+        }
+    }
+
+}
